db3b244c4e845ae8b35b9354cdff8c0227fb92cc
Nicholas Marco <nicoweinandra@gmail.com>
2023-12-14 15:43:57 +0700

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,62 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'frontend',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { 
+        rel: 'icon', 
+        type: 'image/x-icon', 
+        href: '/favicon.ico' 
+      },
+      { 
+        rel: 'stylesheet', 
+        href: '/css/bootstrap.min.css' 
+      },
+      { 
+        rel: 'stylesheet', 
+        href: '/css/custom.css' 
+      }
+    ],
+    script: [
+      {
+        src: '/js/bootstrap.bundle.min.js',
+        type: 'text/javascript'
+      }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="container">
+      <div class="row justify-content-center mt-5">
+        <div class="col-md-6">
+          <form @submit.prevent="login">
+            <h2 class="text-center mb-4">Sign Up</h2>
+  
+            <div class="mb-3">
+              <label for="Email" class="form-label">Email address</label>
+              <input type="email" v-model="data.email" class="form-control" id="Email" required>
+            </div>
+  
+            <div class="mb-3">
+              <label for="Nama" class="form-label">Nama</label>
+              <input type="text" v-model="data.nama" class="form-control" id="Nama" required>
+            </div>
+
+            <div class="mb-3">
+              <label for="AsalSekolah" class="form-label">Asal Sekolah</label>
+              <input type="text" v-model="data.AsalSekolah" class="form-control" id="AsalSekolah" required>
+            </div>
+
+            <div class="mb-3">
+              <label for="Tanggal_Pendaftaran" class="form-label">Tanggal Pendaftaran</label>
+              <input type="date" v-model="data.Tanggal_Pendaftaran" class="form-control" id="Tanggal_Pendaftaran" required>
+            </div>
+  
+            <button type="submit" class="btn btn-primary w-100 mb-5">Daftar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+export default {
+    data() {
+      return {
+        data: {
+          Nama: '',
+          Email: '',
+          AsalSekolah: '',
+          Tanggal_Pendaftaran: '',
+        },
+      };
+    },
+    methods: {
+      async submitForm() {
+        try {
+          const apiUrl = 'http://localhost:8000/api/pendaftaran';
+  
+          const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.data),
+          });
+  
+          const responseData = await response.json();
+  
+          console.log('Data Pendaftaran Berhasil Dikirim:', responseData);
+  
+          // Reset form setelah pengiriman
+          this.data = {
+            Nama: '',
+            Email: '',
+            Asal_Sekolah: '',
+            Tanggal_Pendaftaran: '',
+          };
+        } catch (error) {
+          console.error('Terjadi kesalahan saat mengirim data:', error);
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  /* You can add custom styles here if needed */
+  </style>
+  
